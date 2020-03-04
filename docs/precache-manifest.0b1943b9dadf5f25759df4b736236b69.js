3f19629a2637401fcab7d65d0e996a50514a3618
Charlie Jackson <charlie@charliejackson.com>
2020-03-04 08:47:41 +0000

    build(app): 🔧 build the app into the docs folder for deployment
    branch master

---
+++ docs/precache-manifest.0b1943b9dadf5f25759df4b736236b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622166cc9eaae0cdbd074c5dd9a3409c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be29659a6d4e7e0afb24",
+    "url": "/static/js/2.c6c9c15c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.c6c9c15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c81b36ebbdf553fb6cc",
+    "url": "/static/js/main.795b54b7.chunk.js"
+  },
+  {
+    "revision": "2a3b6c12db55fc4dfb16",
+    "url": "/static/js/runtime-main.770db21f.js"
+  }
+]);
